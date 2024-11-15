9e62cef064b3cdeb8f5857305ec30b06819f22a9
PoojaSharmasuper <dhakadsonu196@gmail.com>
2024-11-15 10:44:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "codeflix_bots"
+API_ID       = 27696177
+API_HASH     = "0c44906a4feff3b947db76dfa7c57d88"
+BOT_TOKEN    = "5918038913:AAFCCR2vyHyazAuQjkIKSBFISOjKxt_rufU"
+SESSION      = "BQD6zg0ACAkYIBVTJkoEaPaiwoWomk6bcGzrpOsFMkFRJp5e0IrDliAu2D-zfM-0vxBEFhWPfC2VHmCx5IjMUyvEZfbBwxM1Zszv6EezbAGlKkhg66IpnxALr5KeO7g-2kbHoRsvul0amWJKa_V0LHTmxY2BPNV_adHlFCyVLO2RVrtd9hc0kVaXEMhDxlaXmAEQZNNeK3EYutYkLJk9OqFT4pprY3SlDM13DCNcdP1OWEPV0WCDnOx6UGFJjaepVQ4vrM6DSQhnBK_gM11SNPOmnNGzkV7BkvRvzmJezudVZ2W8dMh_F9tPU0YCV1Cty-Hj5-gdchAnHJOtCglZj8VOsv8_xgAAAAG6nhFKAA"
+DATABASE_URI = "mongodb+srv://Spiderman:ohWHjNXG9VedoC5R@cluster0.uuauf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002116984433
+ADMIN        = 852190589
+CHANNEL      = "Super4crazy"
